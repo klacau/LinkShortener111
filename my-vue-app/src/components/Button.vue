3e6f582ae163c2f8ca118b4a8c9df8fd96dbd275
Романов Данил <danielo.sarcapone@gmail.com>
2024-05-27 18:13:38 +0300

    кнопка

---
+++ my-vue-app/src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn" type="button">
+        {{label}}
+    </button>
+</template>
+
+<script setup lang="ts">
+interface ButtonProps {
+    label: string;
+}
+
+const props = defineProps<ButtonProps>();
+
+</script>
+
+<style>
+    .btn{
+        font-size: 14px;
+        font-family: Inter;
+        font-weight: 600;
+        line-height: 18px;
+        letter-spacing: 2%;
+        background-color: #1A243F;
+        color: #fff;
+        padding: 15px 26px;
+        border-radius: 16px;
+        border: 0;
+        outline: none;
+    }
+    .btn:hover {
+        cursor: pointer;
+        background-color: #01081b; 
+    }
+</style>
