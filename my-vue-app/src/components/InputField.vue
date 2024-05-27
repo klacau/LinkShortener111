4ed65b7b5d8133bab2adb5cbd899746abcf3b942
klacau <klacau@192.168.1.24>
2024-05-27 21:11:17 +0300

    look2

--- my-vue-app/src/components/InputField.vue
+++ my-vue-app/src/components/InputField.vue
@@ -1,12 +1,27 @@
 <template>
-<input type="text">
+<input class="in" type="text" 
+:value="text"
+@input= "event => $emit('change', event.target!.value)"
+>
 </template>
 
 
 <script setup lang="ts">
+import type {Ref} from 'vue'
+interface InputFieldProps {
+    text: string;
+    }
 
+const props = defineProps<InputFieldProps>()
+const emit = defineEmits<{
+(e: 'change', value: string): void
+}>()
 </script>
 
 <style>
+.in {
+    border-radius: 16px;
+    padding: 15px 16px;
 
+}
 </style>
--- my-vue-app/src/components/MainPage.vue
+++ my-vue-app/src/components/MainPage.vue
@@ -1,11 +1,15 @@
 <template>
 <p>LinkShortener111</p>
 <h2>Вставьте ссылку для сокращения</h2>
-<Button label="Сократить" @click = "() => console.log('Данил гондон ибаный')"/>
+<InputField :text="inputLink" @change="(value) => inputLink = value"/>
+<Button label="Сократить" @click = "() => console.log(inputLink)"/>
 </template>
 
 <script setup lang="ts">
 import Button from './Button.vue'
+import InputField from './InputField.vue';
+import {ref} from 'vue'
+const inputLink = ref('')
 </script>
 
 <style>
