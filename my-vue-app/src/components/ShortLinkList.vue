e622e65c929d5c8386f8e6d2459d5de36cd1d903
klacau <klacau@192.168.1.24>
2024-05-28 20:37:42 +0300

    w

---
+++ my-vue-app/src/components/ShortLinkList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import ShortLinkView from './ShortLinkView.vue';
+interface ShortLink {
+  shortUrl: string;
+  originalUrl: string;
+}
+interface ShortLinkListProps {
+  links: ShortLink[];
+}
+
+const post = defineProps<ShortLinkListProps>();
+</script>
+
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.shortUrl" class="link-item">
+      <ShortLinkView :shortLink="link" />
+    </li>
+  </ul>
+</template>
+
+<style>
+</style>
