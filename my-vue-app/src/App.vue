a03b8ae4ae40dd3ee0df87c24ccf02924429fc6d
klacau <klacau@192.168.1.24>
2024-05-30 15:58:14 +0300

    po

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,7 +2,7 @@
 import type { ShortLink } from './models/ShortLink';
 
 import MainPage from './components/MainPage.vue';
-import { ref, Ref } from 'vue';
+import { ref, Ref} from 'vue';
 
 const linksValue = localStorage.getItem('links');
 const storageLinks: ShortLink[] = linksValue === null ? [] : JSON.parse(linksValue);
@@ -10,6 +10,14 @@ const storageLinks: ShortLink[] = linksValue === null ? [] : JSON.parse(linksVal
 const links: Ref<ShortLink[]> = ref(storageLinks);
 
 function shortenLink(text: string) {
+
+    const reg = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+
+    if (!reg.test(text)) {
+        console.error("Это не ссылка")
+        return;
+    }
+    
     const shortenedUrl = `https://localhost:0000/kS9wQ${Math.round(Math.random() * 9999)}`;
     
     const shortLink: ShortLink = {
